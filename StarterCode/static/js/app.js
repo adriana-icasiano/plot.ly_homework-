03a3905583be4747d0b6c60c126fa7b749868841
adriana-icasiano <81338603+adriana-icasiano@users.noreply.github.com>
2021-07-14 22:12:31 -0400

    build metadata works

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -6,92 +6,109 @@ Feel free to disregard and create your own code */
 //     console.log(data);
 // });
 
-d3.json("samples.json").then(function (data) {
-    console.log(data);
+// d3.json("samples.json").then(function (data) {
+//     console.log(data);
 
-    // Parse and filter data to get sample names
-    let names = []
+//     // Parse and filter data to get sample names
+//     // let names = ["apple"]
+//     let names = [];
+//     let metadata = [];
 
-    data.forEach(function (id) {
-        names.push(id);
-    });
+//     names.push(data.names);
+//     metadata.push(data.metadata);
+//     console.log(names);
+//     console.log(metadata);
 
-    console.log(names)
-});
+// });
 
     // Define function that will run on page load
 function init() {
 
-        // Read json data
-        d3.json("samples.json").then(function (data) {
-            console.log(data);
+    // Read json data
+    d3.json("samples.json").then(function (data){
+     
 
-            // Parse and filter data to get sample names
-            let names = []
+    // Parse and filter data to get sample names
+    let dropdownMenu = d3.select("#selDataset");
+    let samples = data.names;
 
-            data.forEach(function (id) {
-                names.push(id);
-            });
+    // names.push(data.names);
+    samples.forEach((sample)=>{
+        dropdownMenu.append("option").text(sample).property("value",sample);
+     });
+    
+    console.log(samples);
 
-            console.log(names)
+     // Add dropdown option for each sample    
 
-            // Add dropdown option for each sample
-            let dropdownMenu = d3.select("#selDataset");
-            let dataset = dropdownMenu.property("value");
+     // Call functions below using the first sample to build metadata and \
+     //initial plots
+    buildMetadata(samples[0]);
+    buildCharts(samples[0]);
+    optionChanged(samples[0]);
+    });
+}
 
+// Define a function that will create metadata for given sample
+function buildMetadata(sample) {
 
-            // Call functions below using the first sample to build metadata and \
-            //initial plots
-            buildMetadata(data);
-            buildCharts(data);
-            optionChanged(data);
+    // Read the json data
+    d3.json("samples.json").then(function (data) {
 
-        });
+    
+    let metadata = data.metadata;
+    
+    console.log(metadata);
+    
+    let resultArray = metadata.filter(sampleObject => sampleObject.id == sample);
 
-        // Define a function that will create metadata for given sample
-        function buildMetadata(sample) {
+    let result = resultArray[0];
 
-            // Read the json data
-            d3.json("samples.json").then(function (data) {
-                console.log(data);
-            });
+    let demoTable = d3.select("#sample-metadata");
 
-            // Parse and filter the data to get the sample's metadata
-            let metadata = data.map(function (sample) {
-                return sample.metadata
-            });
+    demoTable.html("");
 
-            // Specify the location of the metadata and update it
-            Object.entries(metadata).forEach(([key, value]) => {
-                // append the information to the demographics panel
-                // using the variables key and value
-            })
-        }
+    Object.entries(result).forEach(([key, value]) =>{
+        demoTable.append("p").text(`${key}:${value}`);
+
+        // append the information to the demographics panel
+        // using the variables key and value
+    });   
+// build gauge function
+}); 
+}
+    
+    
+    // Parse and filter the data to get the sample's metadata
+    
+
+    // Specify the location of the metadata and update it
+   
+
 
         // Define a function that will create charts for given sample
-        function buildCharts(sample) {
+function buildCharts(sample) {
 
 
-            // Read the json data
+    // Read the json data
 
-            // Parse and filter the data to get the sample's OTU data
-            // Pay attention to what data is required for each chart
+    // Parse and filter the data to get the sample's OTU data
+    // Pay attention to what data is required for each chart
 
-            // Create bar chart in correct location
+    // Create bar chart in correct location
 
-            // Create bubble chart in correct location
+    // Create bubble chart in correct location
 
-        }
+}
 
+function optionChanged(sample) {
+    // The parameter being passed in this function is new sample id from dropdown menu
 
-        function optionChanged(sample) {
-            // The parameter being passed in this function is new sample id from dropdown menu
+    // Update metadata with newly selected sample
 
-            // Update metadata with newly selected sample
+    // Update charts with newly selected sample
 
-            // Update charts with newly selected sample
+}
 
-        }
-
-        // Initialize dashboard on page load
-        init();
+// Initialize dashboard on page load
+init();
